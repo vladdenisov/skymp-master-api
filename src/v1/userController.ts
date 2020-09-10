99dfd0db6225102a05ac447688a5bcdce1eff47f
Leonid Pospelov <pospelovlm@yandex.ru>
2020-09-10 22:05:03 +0300

    Remove unused sample routes

--- src/v1/userController.ts
+++ src/v1/userController.ts
@@ -1,47 +1,19 @@
 import { Context } from "koa";
 import * as Router from "koa-router";
-import { getManager, Repository, Not, Equal } from "typeorm";
+import { getManager, Repository } from "typeorm";
 import { validate, ValidationError } from "class-validator";
 
 import { User } from "../models/user";
 
 export class UserController {
   static getRouter(): Router {
-    return new Router()
-      .get("/users", UserController.getUsers)
-      .get("/users/:id", UserController.getUser)
-      .post("/users", UserController.createUser)
-      .put("/users/:id", UserController.updateUser)
-      .delete("/users/:id", UserController.deleteUser);
+    return new Router().post("/users", UserController.createUser);
   }
 
   static getRepository(ctx: Context | Router.RouterContext): Repository<User> {
     return getManager(
       (ctx as Record<string, string>)["connectionName"]
     ).getRepository(User);
-  }
-
-  static async getUsers(ctx: Context | Router.RouterContext): Promise<void> {
-    const userRepository: Repository<User> = getManager().getRepository(User);
-
-    const users: User[] = await userRepository.find();
-
-    ctx.status = 200;
-    ctx.body = users;
-  }
-
-  static async getUser(ctx: Context | Router.RouterContext): Promise<void> {
-    const userRepository: Repository<User> = getManager().getRepository(User);
-
-    const user = await userRepository.findOne(ctx.params.id);
-
-    if (user) {
-      ctx.status = 200;
-      ctx.body = user;
-    } else {
-      ctx.status = 400;
-      ctx.body = "User not found";
-    }
   }
 
   static async createUser(ctx: Context | Router.RouterContext): Promise<void> {
@@ -68,64 +40,6 @@ export class UserController {
       const userCreated = await userRepository.save(user);
       ctx.status = 201;
       ctx.body = userCreated;
-    }
-  }
-
-  static async updateUser(ctx: Context | Router.RouterContext): Promise<void> {
-    const userRepository: Repository<User> = getManager().getRepository(User);
-
-    const user = await userRepository.findOne(ctx.params.id);
-
-    if (!user) {
-      ctx.status = 400;
-      ctx.body = "The user you are trying to retrieve doesn't exist in the db";
-    } else {
-      if (ctx.request.body.name) {
-        user.name = ctx.request.body.name;
-      }
-
-      if (ctx.request.body.email) {
-        user.email = ctx.request.body.email;
-      }
-      if (ctx.request.body.password) {
-        user.password = ctx.request.body.password;
-      }
-
-      const errors: ValidationError[] = await validate(user);
-
-      if (errors.length > 0) {
-        ctx.status = 400;
-        ctx.body = errors;
-      } else if (!(await userRepository.findOne(user.id))) {
-        ctx.status = 400;
-        ctx.body = "The user you are trying to update doesn't exist in the db";
-      } else if (
-        await userRepository.findOne({
-          id: Not(Equal(user.id)),
-          email: user.email
-        })
-      ) {
-        ctx.status = 400;
-        ctx.body = "The specified e-mail address already exists";
-      } else {
-        const userUpdated = await userRepository.save(user);
-        ctx.status = 201;
-        ctx.body = userUpdated;
-      }
-    }
-  }
-
-  static async deleteUser(ctx: Context | Router.RouterContext): Promise<void> {
-    const userRepository: Repository<User> = getManager().getRepository(User);
-
-    const userToRemove = await userRepository.findOne(ctx.params.id);
-
-    if (!userToRemove) {
-      ctx.status = 400;
-      ctx.body = "User doesn't exist";
-    } else {
-      await userRepository.remove(userToRemove);
-      ctx.status = 204;
     }
   }
 }
