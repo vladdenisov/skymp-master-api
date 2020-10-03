5767fa042156f7b30c2c63ccf176f9f27fd63a52
Leonid Pospelov <pospelovlm@yandex.ru>
2020-10-04 02:59:07 +0300

    Make app configurable via environment variables

--- src/cfg.ts
+++ src/cfg.ts
@@ -12,6 +12,26 @@ export interface Config {
 
 const getConfig = (): Config => {
   const cfgPath = "./src/config.json";
+
+  if (process.env.USE_ARGS) {
+    const config = {};
+    [
+      "PORT",
+      "JWT_SECRET",
+      "DB_URL",
+      "EMAIL_USER",
+      "EMAIL_PASS",
+      "STATS_CSV_PATH",
+      "IS_GITHUB_ACTION"
+    ].forEach((optionName) => {
+      if (process.env[optionName] !== undefined) {
+        config[optionName] = process.env[optionName];
+      }
+    });
+    fs.writeFileSync(cfgPath, JSON.stringify(config));
+    console.log("Using config from env");
+  }
+
   const exampleConfig = fs.readFileSync("./src/config-example.json", "utf-8");
   if (!fs.existsSync(cfgPath)) fs.writeFileSync(cfgPath, exampleConfig);
   const cfg: Config = JSON.parse(fs.readFileSync(cfgPath, "utf-8"));
