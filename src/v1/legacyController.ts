97cdec3cf854c4cf5759696228c0d11c4e3349dd
Leonid Pospelov <pospelovlm@yandex.ru>
2021-04-02 08:29:12 +0300

    Increase hardcoded playerLimit

--- src/v1/legacyController.ts
+++ src/v1/legacyController.ts
@@ -8,7 +8,7 @@ import * as cfg from "../cfg";
 
 const ipAndPortRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$/;
 
-const playerLimit = 100;
+const playerLimit = 50000;
 
 export interface ServerOnline {
   online: number;
